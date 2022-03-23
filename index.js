9cc823728949db726efab41abb481aa2d49296e7
William Luck <luck.william.h@gmail.com>
2022-03-23 10:23:26 -0500

    commit tree 64ed5f993c76beb3f6c1c79fd1bc0e44237a6b9b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+const dodger = document.getElementById('dodger')
+document.addEventListener('keydown', e => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+ 
+function moveDodgerLeft (event) {
+    let position = parseInt(dodger.style.left, 10)
+    if (position > 0) {
+        position = position - 10;
+        dodger.style.left = `${position}px`
+    }
+}
+
+function moveDodgerRight() {
+    let position = parseInt(dodger.style.left, 10)
+    if (position < 360) {
+        position = position + 10;
+        dodger.style.left = `${position}px`
+    }
+}
+    
